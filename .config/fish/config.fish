1512b92fafd568843bf3fef81317c626e30cbe3b
Yuki <github@risaton.net>
2024-04-04 20:02:54 +0900

    add fish configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+	# Commands to run in interactive sessions can go here
+	alias la 'ls -alG'
+	alias gls 'gls --color=auto'
+	alias lh 'ls -alh'
+	set -gx HOMEBREW_PREFIX "/opt/homebrew";
+	set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+	set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+	fish_add_path -gP "/opt/homebrew/bin" "/opt/homebrew/sbin";
+	! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+	! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+	function useport
+		set -a PATH /opt/local/bin
+	end
+	fish_add_path -gP /opt/homebrew/opt/coreutils/libexec/gnubin
+	fish_add_path /Users/user/Library/Application Support/JetBrains/Toolbox/scripts
+	fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin
+	fish_add_path /usr/local/bin
+end
